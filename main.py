dbb98f6b8087b09a5a45ab38198fd419f34c4232
2633dot <stawickikamil54@gmail.com>
2024-04-23 15:16:24 +0200

    cooked letter counting implementation

--- main.py
+++ main.py
@@ -9,13 +9,19 @@ def count_words(split_book):
             count = count + 1
     return count
 
+def sort_on(tuple):
+    return tuple[1]
+
 def count_letters(book):
     letter_count_dict = {}
     book = book.lower()
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     for letter in alphabet:
         letter_count_dict[letter] = book.count(letter)
-    return letter_count_dict
+    
+    letter_count_list = list(letter_count_dict.items())
+    letter_count_list.sort(reverse=True, key=sort_on)  
+    return letter_count_list
 
 
 def main():
@@ -23,7 +29,10 @@ def main():
         file_contents = f.read()
         split_file = split_book(file_contents)
         letter_count = count_letters(file_contents)
+    
     print(f"{count_words(split_file)} words found in opened file")
-    print(f"{letter_count}")
+    for tuple in letter_count:
+        print(f"The letter {tuple[0]} has been used {tuple[1]} times")
+
 
 main()
